f79e895d88f8e1fda5ecccf5903cd16710ec51ed
Sakib91 <birdhunterx91@gmail.com>
2023-12-24 14:45:09 +0600

    location-3

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const location_router=require("./VotingLocation/VotingLocationRouter")
 app.get('/', (req, res) => {
   res.send('Hello World-2!')
 })
+app.get("/sex",(req,res)=>{res.send("bohut tej")})
 app.use("/api/location",location_router)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
