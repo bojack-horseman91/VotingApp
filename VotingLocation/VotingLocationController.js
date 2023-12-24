b45f5daddc196f12ebb572dada8982d5634cb183
Sakib91 <birdhunterx91@gmail.com>
2023-12-24 14:55:55 +0600

    location-3

--- VotingLocation/VotingLocationController.js
+++ VotingLocation/VotingLocationController.js
@@ -1,13 +1,13 @@
 const location_model=require("./VotingLocationModel")
-const getAllLocation=(req,res)=>{
-    // try {
-    //     const all_location=await location_model.find()
-    //     res.status(200).json(all_location)
-    // } catch (error) {
-    //     res.status(500).json(error)
-    // } 
-    console.log("ok")
-    res.send("hey sakib")
+const getAllLocation=async(req,res)=>{
+    try {
+        const all_location=await location_model.find()
+        res.status(200).json(all_location)
+    } catch (error) {
+        res.status(500).json(error)
+    } 
+    // console.log("ok")
+    // res.send("hey sakib")
 }
 
 const makeLocation=async(req,res)=>{
