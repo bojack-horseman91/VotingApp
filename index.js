94a4365971cc631eaa6b9738aa1e20a918747ccc
Sakib91 <birdhunterx91@gmail.com>
2023-12-24 14:39:36 +0600

    location

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000|process.env.PORT
+const location_router=require("./VotingLocation/VotingLocationRouter")
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.use("/api/location",location_router)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
