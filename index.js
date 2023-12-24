2a0fcf698533edab4a42ab089a915f3b154de558
Sakib91 <birdhunterx91@gmail.com>
2023-12-24 14:51:16 +0600

    location-3

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000|process.env.PORT
 const location_router=require("./VotingLocation/VotingLocationRouter")
+const controller=require('./VotingLocation/VotingLocationController')
+const parser=require('body-parser')
 
-app.get('/', (req, res) => {
-  res.send('Hello World-3!')
-})
+
+app.get("/",controller.getAllLocation)
+// app.get('/', (req, res) => {
+//   res.send('Hello World-3!')
+// })
 app.get("/sex",(req,res)=>{res.send("bohut tej")})
 app.use("/api/location",location_router)
 app.listen(port, () => {
