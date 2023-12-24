429b31605c8bf0c6ab8ec04202c66c8fd4e0089c
Sakib91 <birdhunterx91@gmail.com>
2023-12-24 15:13:56 +0600

    idk

--- index.js
+++ index.js
@@ -6,28 +6,28 @@ const location_router=require("./VotingLocation/VotingLocationRouter")
 const controller=require('./VotingLocation/VotingLocationController')
 const parser=require('body-parser')
 
-const Voting_location_schema=mangoos.Schema({
-    location:{type:String},
-    imageURL:{type:String},
-    presiding_officer:{type:String},
-    police_officer:{type:String},
-})
-
-location_model=mangoos.model("Voting_Location_Schema",Voting_location_schema)
-app.get("/",(req,res)=>{
-    // console.log("ok")
-    // try {
-    //     const all_location=location_model.find()
-    //     res.status(200).json(all_location)
-    // } catch (error) {
-    //     res.status(500).json(error)
-    // } 
-    // console.log("ok")
-    res.send("hey sakib")
-})
-// app.get('/', (req, res) => {
-//   res.send('Hello World-3!')
+// const Voting_location_schema=mangoos.Schema({
+//     location:{type:String},
+//     imageURL:{type:String},
+//     presiding_officer:{type:String},
+//     police_officer:{type:String},
 // })
+
+// location_model=mangoos.model("Voting_Location_Schema",Voting_location_schema)
+// app.get("/",(req,res)=>{
+//     // console.log("ok")
+//     // try {
+//     //     const all_location=location_model.find()
+//     //     res.status(200).json(all_location)
+//     // } catch (error) {
+//     //     res.status(500).json(error)
+//     // } 
+//     // console.log("ok")
+//     res.send("hey sakib")
+// })
+app.get('/', (req, res) => {
+  res.send('Hello World-3!')
+})
 app.get("/sex",(req,res)=>{res.send("bohut tej")})
 app.use("/api/location",location_router)
 app.listen(port, () => {
