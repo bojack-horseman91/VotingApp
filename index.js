a39b70cc24ff080e18ef72934df787ea0da2f120
Sakib91 <birdhunterx91@gmail.com>
2023-12-24 15:07:02 +0600

    location-3

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express')
+const mangoos = require("mongoose")
 const app = express()
 const port = 3000|process.env.PORT
 const location_router=require("./VotingLocation/VotingLocationRouter")
 const controller=require('./VotingLocation/VotingLocationController')
 const parser=require('body-parser')
 
+const Voting_location_schema=mangoos.Schema({
+    location:{type:String},
+    imageURL:{type:String},
+    presiding_officer:{type:String},
+    police_officer:{type:String},
+})
 
-app.get("/",controller.getAllLocation)
+location_model=mangoos.model("Voting_Location_Schema",Voting_location_schema)
+app.get("/",async(req,res)=>{
+    console.log("ok")
+    try {
+        const all_location=await location_model.find()
+        res.status(200).json(all_location)
+    } catch (error) {
+        res.status(500).json(error)
+    } 
+    // console.log("ok")
+    // res.send("hey sakib")
+})
 // app.get('/', (req, res) => {
 //   res.send('Hello World-3!')
 // })
